8c4acf7167b45553f88a074f8b7c9067b1bcc0bb
Primgdev <primgdev@gmail.com>
2023-09-14 18:32:57 +1000

    push

---
+++ js/audio.js
@@ -0,0 +1,20 @@
+﻿createAudio = function(filename) {
+    var audio = document.createElement('audio');
+    var source = document.createElement('source');
+    source.src = filename;
+    audio.appendChild(source);
+    return audio;
+};
+
+playAudio=function(filename) {
+    var audio = new createAudio(filename);
+    audio.play();
+    
+
+}
+
+stopAudio=function(elem)
+{
+    elem.muted = true;
+    elem.pause();
+}
